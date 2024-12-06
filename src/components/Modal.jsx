a665391b0fad46d82fefe7ba59609df9c93f2568
Ksenia Bondarevich <bondarevicksenia@gmail.com>
2024-12-06 16:04:45 +0300

    last commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, title, link }) => {
   return (
       <div className="modal-overlay">
           <div className="modal-content">
-              <iframe src={link} width="800" height="600" title={title} />
+              <iframe src={link} width="800" height="600" title={title}/>
               <button className="modal-close-button" onClick={onClose}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
